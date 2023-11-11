cc6cfdd516afc6421612e4d7149d43f11992cef4
Asad373 <mianasad373@gmail.com>
2023-11-11 13:12:24 +0500

    Basic Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Profile from './components/profile';
+
+function App() {
+  return (
+    <>
+    <Profile></Profile> 
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/profile.js
@@ -0,0 +1,19 @@
+
+import "./css/Profile.css"
+
+ 
+const Profile = ()=>{
+
+    return(
+        <div class = "mainContainer">
+           <div class = "imageContainer">
+            </div>
+           <div class = "profileContainer">
+            <div class = "name">M.Asad</div>
+            <div className="tagLine">FullStack Engineer </div>
+           </div>
+        </div>
+    );
+}
+
+export default Profile
